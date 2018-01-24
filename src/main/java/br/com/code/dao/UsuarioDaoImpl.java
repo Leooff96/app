a7d448dbeae877b3a63128a1400cfa3af88fdea8
Leonardo Oliveira De Freitas <lofreitas2@stefanini.com>
2018-01-24 15:09:45 -0200

    V1.0.5

--- src/main/java/br/com/code/dao/UsuarioDaoImpl.java
+++ src/main/java/br/com/code/dao/UsuarioDaoImpl.java
@@ -1,69 +1,48 @@
 package br.com.code.dao;
 
-import java.time.LocalDate;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.stream.Collectors;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
-import br.com.code.domain.TipoSexo;
 import br.com.code.domain.Usuario;
 
 @Repository
+@Transactional
 public class UsuarioDaoImpl implements UsuarioDao {
 
-	private static List<Usuario> us;
+	@PersistenceContext
+	private EntityManager entityManager;
 	
-	public UsuarioDaoImpl() {
-		createUserList();
-	}
-	
-    private List<Usuario> createUserList() {
-        if (us == null) {
-            us = new LinkedList<>();
-            us.add(new Usuario(System.currentTimeMillis()+1L, "Ana", "da Silva",TipoSexo.FEMININO,LocalDate.of(1992, 5, 10)));
-            us.add(new Usuario(System.currentTimeMillis()+2L, "Luiz", "dos Santos",TipoSexo.MASCULINO,LocalDate.of(1992, 4, 1)));
-            us.add(new Usuario(System.currentTimeMillis()+3L, "Mariana", "Mello",TipoSexo.FEMININO,LocalDate.of(1992, 3, 28)));
-            us.add(new Usuario(System.currentTimeMillis()+4L, "Caren", "Pereira"));
-            us.add(new Usuario(System.currentTimeMillis()+5L, "Sonia", "Fagundes"));
-            us.add(new Usuario(System.currentTimeMillis()+6L, "Norberto", "de Souza"));
-        }
-        return us;
-    }
-
 	@Override
 	public void salvar(Usuario usuario) {
-		usuario.setId(System.currentTimeMillis());
-		us.add(usuario);
+		entityManager.persist(usuario);
 	}
 
 	@Override
 	public void editar(Usuario usuario) {
-		us.stream()
-			.filter((u) -> u.getId().equals(usuario.getId()))
-			.forEach((u) -> {
-				u.setNome(usuario.getNome());
-				u.setSobrenome(usuario.getSobrenome());
-				u.setData(usuario.getData());
-				u.setSexo(usuario.getSexo());
-			});		
+		entityManager.refresh(usuario);
 	}
 
 	@Override
 	public void excluir(Long id) {
-		us.removeIf((u) -> u.getId().equals(id));		
+		
 	}
 
 	@Override
 	public Usuario getId(Long id) {
-		return us.stream()
-				.filter((u) -> u.getId().equals(id))
-				.collect(Collectors.toList()).get(0);
+		return null;
 	}
 
+	@Transactional(readOnly = true)
 	@Override
 	public List<Usuario> getTodos() {
-		return us;
-	}	
+		String jpql = "from Usuario u";
+		return entityManager.createQuery(jpql,Usuario.class).getResultList();
+	}
+	
+
 }
--- src/main/java/br/com/code/domain/Usuario.java
+++ src/main/java/br/com/code/domain/Usuario.java
@@ -1,7 +1,16 @@
 package br.com.code.domain;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -9,20 +18,31 @@ import org.hibernate.validator.constraints.NotBlank;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.format.annotation.DateTimeFormat.ISO;
 
-public class Usuario {
+@Entity
+@Table(name = "usuarios")
+public class Usuario implements Serializable {
 
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
+	@Column(nullable=false, length=50)
 	@NotBlank
 	@Size(min=3,max=50)
 	private String nome;
 	
+	@Column(nullable=false, length=50)
 	@NotBlank
 	@Size(min=3,max=50, message="Campo requerido entre {min} e {max}")
 	private String sobrenome;
 	
+	@Column(name="tipo_sexo",nullable=false)
+	@Enumerated(EnumType.STRING)
 	private TipoSexo sexo;
 	
+	@Column(name="data_nascimento",nullable=false)
 	@NotNull(message="O campo de data é requerido")
 	@DateTimeFormat(iso = ISO.DATE)
 	private LocalDate data;
@@ -30,6 +50,7 @@ public class Usuario {
 	public Usuario() {
 		super();
 	}	
+
 
 	public Usuario(Long id, String nome, String sobrenome, TipoSexo sexo, LocalDate data) {
 		super();
@@ -100,5 +121,29 @@ public class Usuario {
 		return "Usuario [id=" + id + ", nome=" + nome + ", sobrenome=" + sobrenome + "]";
 	}
 	
-	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
 }
